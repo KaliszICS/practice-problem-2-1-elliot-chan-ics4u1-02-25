f96185ab11f0743a7689cfde1777e9c002946d28
Elliot Chan <elliot.chan@student.tdsb.on.ca>
2025-03-20 13:46:13 +0000

    Finish practice problem

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -4,6 +4,32 @@ public class PracticeProblem {
 
 	}
 
-	
+	public static int find(int[] array, int number) {
+		for(int i = 0; i < array.length; i++) {
+			if(array[i] == number)
+				return i;
+		}
+		return -1;
+	}
+	public static int findLast(String[] array, String string) {
+
+		for(int i = array.length - 1; i >= 0; i--) {
+			if(array[i].equals(string))
+				return i;
+		}
+		return -1;
+	}
+	public static int findSecond(char[] array, char character) {
+		int counter = 0;
+		for(int i = 0; i < array.length; i++) {
+			if(array[i] == character) {
+				counter++;
+				if(counter == 2 || (counter == 1 && i == array.length - 1))
+					return i;
+			}
+		}
+		return -1;
+		
+	}
 
 }
